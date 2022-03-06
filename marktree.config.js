b234897057eadb04a10fc03a410660cf4a922b70
magnetenstad <tenstad.magne@gmail.com>
2022-03-06 15:44:40 +0100

    Add custom layout and styles

--- marktree.config.js
+++ marktree.config.js
@@ -2,6 +2,8 @@
 export default {
   source: 'example',
   dest: 'docs',
-  htmlBase: 'base.html',
-  htmlReplace: '<!-- marktree -->'
+  htmlLayout: 'layout.html',
+  insertMarkdown: '<!-- markdown -->',
+  insertStyles: '<!-- styles -->',
+  cssStyles: 'styles.css',
 }
--- src/marktree.js
+++ src/marktree.js
@@ -2,12 +2,12 @@ import config from '../marktree.config.js'
 import { File, Directory } from './files.js'
 import MarkdownIt from 'markdown-it'
 const md = new MarkdownIt();
-const htmlBase = File.read(config.htmlBase).data
 
 export default class MarkTree {
 
   constructor() {
     this.markdown = Directory.read(config.source);
+    console.log(this.markdown.toString());
     this.markdown.name = config.dest
     link(this.markdown)
   }
@@ -25,60 +25,118 @@ export default class MarkTree {
   }
 }
 
+
+/**
+ * 
+ * @param {Directory} directory 
+ */
 function writeHtml(directory) {
   const htmlDirectory = makeHtmlDirectory(directory)
+  console.log(htmlDirectory.toString());
   htmlDirectory.write();
 }
 
-function makeHtmlDirectory(mdDirectory) {
+
+/**
+ * 
+ * @param {Directory} mdDirectory 
+ * @param {*} htmlLayout 
+ * @param {*} cssStyles 
+ * @returns 
+ */
+function makeHtmlDirectory(mdDirectory, htmlLayout=null, cssStyles=null) {
+  // Create a new directory
   const htmlDirectory = new Directory(mdDirectory.name)
-  mdDirectory.files.forEach((mdFile) => {
-    const htmlRender = md.render(mdFile.data.replaceAll('.md)', '.html)'))
-    const htmlData = htmlBase.replaceAll(config.htmlReplace, htmlRender)
-    const htmlFile = new File(mdFile.name.replaceAll('.md', '.html'), htmlData)
-    htmlDirectory.files.push(htmlFile)
+
+  // Get htmlLayout and cssStyles
+  const newLayout = mdDirectory.getFile(config.htmlLayout)
+  if (newLayout) {
+    htmlLayout = newLayout.data
+  }
+  const newStyles = mdDirectory.getFile(config.cssStyles)
+  if (newStyles) {
+    cssStyles = newStyles
+  } 
+
+  // Copy files to new directory
+  mdDirectory.files.forEach((file) => {
+    if (file.name.endsWith('.html')) return
+    if (file.name.endsWith('.md')) {
+      // Markdown files are converted to html
+      const htmlRender = md.render(file.data.replaceAll('.md)', '.html)'))
+      const htmlStyles = `<link rel="stylesheet" href="${cssStyles}">`
+      const htmlData = htmlLayout
+          .replaceAll(config.insertMarkdown, htmlRender)
+          .replaceAll(config.insertStyles, htmlStyles)
+      const htmlFile = new File(file.name.replaceAll('.md', '.html'), htmlData)
+      htmlDirectory.files.push(htmlFile)
+    } else {
+      htmlDirectory.files.push(new File(file.name, file.data))
+    }
   })
+
+  // Recursively create html subdirectories
   mdDirectory.directories.forEach((mdSubDir) => {
-    htmlDirectory.directories.push(makeHtmlDirectory(mdSubDir))
+    htmlDirectory.directories.push(
+        makeHtmlDirectory(mdSubDir, htmlLayout, '../' + cssStyles))
   })
+
   return htmlDirectory
 }
 
-function link(directory, parentDirectory=null) {
 
+/**
+ * 
+ * @param {Directory} directory 
+ * @param {Directory} parentDirectory 
+ */
+function link(directory, parentDirectory=null) {
+  // Create link to directory index.md
   directory.files.forEach((file) => {
-    if (file.name === 'index.md') return
-    let markdown = `[${directory.name}](./index.md)\n`
-    file.data = markdown + file.data
+    if (file.name === 'index.md' || !file.name.endsWith('.md')) return
+    file.data = `[${directory.name}](./index.md)\n` + file.data
   })
 
+  // Get existing index.md
   const indexFile = directory.getFile('index.md')
   const indexData = indexFile ? indexFile.data : ''
   directory.removeFile(indexFile)
-  let markdown = ''
+  let indexMd = ''
 
+  // Create link to parent directory index.md
   if (parentDirectory) {
-    markdown += `[${parentDirectory.name}](../index.md)\n`
+    indexMd += `[${parentDirectory.name}](../index.md)\n`
   }
-  markdown += `\n# ${directory.name}\n\n`
-
-  if (directory.directories.length) {
-    markdown += `## Directories\n`
-    directory.directories.forEach((directory) => {
-      markdown += `[${directory.name}](./${directory.name}/index.md)\n`
-    })
-  }
-
-  if (directory.files.length) {
-    markdown += `## Files\n`
-    directory.files.forEach((file) => {
-      markdown += `[${file.name}](${file.name})\n`
-    })
-  }
-
-  directory.files.push(new File('index.md', markdown + indexData))
   
-  directory.directories.forEach((dir) => {
-    link(dir, directory)
+  // Create header
+  indexMd += `\n# ${directory.name}\n\n`
+
+  // Create links to subdirectories
+  if (directory.directories.length) {
+    indexMd += `## Directories\n`
+    directory.directories.forEach((directory) => {
+      indexMd += `[${directory.name}](./${directory.name}/index.md)\n`
+    })
+  }
+
+  // Create links to files
+  if (directory.files.length) {
+    let count = 0
+    directory.files.forEach((file) => {
+      if (file.name == config.htmlLayout || file.name == config.cssStyles) return
+      if (!count) {
+        indexMd += `## Files\n`
+        count++
+      }
+      indexMd += `[${file.name}](${file.name})\n`
+    })
+  }
+
+  // Add index.md to directory
+  directory.files.push(new File('index.md', indexMd + indexData))
+  
+  // Recursively link subdirectories
+  directory.directories.forEach((subDirectory) => {
+    link(subDirectory, directory)
   })
 }
