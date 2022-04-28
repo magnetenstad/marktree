3f170159d0924479848c0ad66c92dd546fd0078d
magnetenstad <tenstad.magne@gmail.com>
2022-04-28 17:49:45 +0200

    Fix include

--- README.md
+++ README.md
@@ -3,32 +3,44 @@
 marktree is a static site generator, using markdown. It works with GitHub pages out of the box.
 
 ## Get started
+
 [create-marktree](https://github.com/magnetenstad/create-marktree) is the quickest way to start a marktree project.
 
 ### Doing it manually
+
 1. Install marktree
+
 ```sh
 npm i marktree
 ```
+
 2. Create a `markdown/`-folder and fill it with markdown files
 3. Add the build script to `package.json`
+
 ```diff
 "scripts": {
 +  "build": "marktree"
 },
 ```
+
 4. Build your site
+
 ```sh
 npm run build
 ```
+
 5. Host the contents of `docs/`
 
 ### Hot reload
+
 1. Install nodemon
+
 ```
 npm i nodemon -D
 ```
+
 2. Add the dev script to `package.json` and update `main`
+
 ```diff
 +"main": "node_modules/marktree/src/index.js",
 "scripts": {
@@ -36,14 +48,18 @@ npm i nodemon -D
    "build": "marktree"
 },
 ```
+
 3. Add the `nodemon.json` config file
+
 ```json
 {
   "watch": ["src/", "markdown/"],
   "ext": "md, js"
 }
 ```
+
 4. Run dev
+
 ```sh
 npm run dev
 ```
@@ -51,7 +67,9 @@ npm run dev
 ## Configuration
 
 ### Configure marktree
+
 Configure marktree by adding a `marktree.config.json` config file. The following are default values.
+
 ```json
 {
   "source": "markdown",
@@ -65,18 +83,22 @@ Configure marktree by adding a `marktree.config.json` config file. The following
   "insertMarkdown": "<!-- insert:markdown -->",
   "insertStyles": "<!-- insert:styles -->",
   "insertTitle": "<!-- insert:title -->",
-  "insertIcon": "<!-- insert:icon -->",
+  "insertIcon": "<!-- insert:icon -->"
 }
 ```
 
 ### Styling
+
 Add a `styles.css` file to the directory you want to be styled. Styles are also applied all subdirectories.
 
 ### Icon
+
 Add a `favicon.ico` file to the directory you want to include the icon. The icon is also applied to all subdirectories.
 
 ### YAML
+
 Add file metadata with YAML. THe following attributes are supported
+
 ```yaml
 ---
 title:
--- src/marktree.js
+++ src/marktree.js
@@ -1,36 +1,41 @@
-import { File, Directory } from 'virtual-file-system'
-import MarkdownIt from 'markdown-it'
-import MarkdownKatex from '@iktakahiro/markdown-it-katex'
-import MarkdownHighlight from 'markdown-it-highlightjs'
-import MarkdownInclude from 'markdown-it-include'
-import { defaultConfig, defaultHtmlLayout, defaultCssStyles }
-    from './defaults.js'
-import metadataParser from 'markdown-yaml-metadata-parser'
+import { File, Directory } from 'virtual-file-system';
+import MarkdownIt from 'markdown-it';
+import MarkdownKatex from '@iktakahiro/markdown-it-katex';
+import MarkdownHighlight from 'markdown-it-highlightjs';
+import MarkdownInclude from 'markdown-it-include';
+import {
+  defaultConfig,
+  defaultHtmlLayout,
+  defaultCssStyles,
+} from './defaults.js';
+import metadataParser from 'markdown-yaml-metadata-parser';
 
-export { buildMarktree }
+export { buildMarktree };
 
-const config = defaultConfig
-const configFile = File.read('marktree.config.json')
+const config = defaultConfig;
+const configFile = File.read('marktree.config.json');
 if (configFile) {
-  Object.assign(config, JSON.parse(configFile.data))
+  Object.assign(config, JSON.parse(configFile.data));
 }
 const md = new MarkdownIt();
-md.use(MarkdownInclude,
-    { includeRe: /\n#include!(.+)/, bracesAreOptional: true })
-md.use(MarkdownKatex, { "throwOnError" : false, "errorColor" : " #cc0000" });
+md.use(MarkdownInclude, {
+  includeRe: /\n#include!(.+)/,
+  bracesAreOptional: true,
+});
+md.use(MarkdownKatex, { throwOnError: false, errorColor: ' #cc0000' });
 md.use(MarkdownHighlight, { inline: true });
 
 function buildMarktree() {
-  console.log('Starting build...')
+  console.log('Starting build...');
 
   const mdDirectory = Directory.read(config.source, config.exclude);
 
-  if (config.include) {
+  if (config.include && config.include.length) {
     for (let dir of mdDirectory.directories) {
       if (!config.include.includes(dir.name)) {
-        const index = mdDirectory.directories.indexOf(dir)
+        const index = mdDirectory.directories.indexOf(dir);
         if (index > -1) {
-          mdDirectory.directories.splice(index, 1)
+          mdDirectory.directories.splice(index, 1);
         }
       }
     }
@@ -38,16 +43,16 @@ function buildMarktree() {
 
   console.log('[Read] ' + mdDirectory.toString());
 
-  editMarkdown(mdDirectory)
+  editMarkdown(mdDirectory);
 
-  console.log('Edited markdown')
+  console.log('Edited markdown');
 
-  const htmlDirectory = buildHtml(mdDirectory)
-  htmlDirectory.name = config.dest
+  const htmlDirectory = buildHtml(mdDirectory);
+  htmlDirectory.name = config.dest;
 
-  console.log('Rendered html')
+  console.log('Rendered html');
 
-  htmlDirectory.write()
+  htmlDirectory.write();
 
   console.log('[Write] ' + htmlDirectory.toString());
   console.log('Successfully finished building!');
@@ -55,172 +60,180 @@ function buildMarktree() {
 
 function editMarkdown(directory) {
   if (!directory.getFile(config.htmlLayout)) {
-    directory.files.push(new File(config.htmlLayout, defaultHtmlLayout))
+    directory.files.push(new File(config.htmlLayout, defaultHtmlLayout));
   }
   if (!directory.getFile(config.cssStyles)) {
-    directory.files.push(new File(config.cssStyles, defaultCssStyles))
+    directory.files.push(new File(config.cssStyles, defaultCssStyles));
   }
-  readMetadata(directory)
+  readMetadata(directory);
   if (config.autoLink) {
-    linkMarkdown(directory)
+    linkMarkdown(directory);
   }
 }
 
 /**
- * 
- * @param {Directory} mdDirectory 
- * @param {*} htmlLayout 
- * @param {*} cssStyles 
- * @param {*} icon 
- * @returns 
+ *
+ * @param {Directory} mdDirectory
+ * @param {*} htmlLayout
+ * @param {*} cssStyles
+ * @param {*} icon
+ * @returns
  */
-function buildHtml(mdDirectory, htmlLayout=null, cssStyles=[], icon=null) {
+function buildHtml(
+  mdDirectory,
+  htmlLayout = null,
+  cssStyles = [],
+  icon = null
+) {
   // Create a new directory
-  const htmlDirectory = new Directory(mdDirectory.name)
+  const htmlDirectory = new Directory(mdDirectory.name);
 
   // Get htmlLayout and cssStyles
-  const newLayout = mdDirectory.getFile(config.htmlLayout)
+  const newLayout = mdDirectory.getFile(config.htmlLayout);
   if (newLayout) {
-    htmlLayout = newLayout.data
+    htmlLayout = newLayout.data;
   }
-  const newStyles = mdDirectory.getFile(config.cssStyles)
+  const newStyles = mdDirectory.getFile(config.cssStyles);
   if (newStyles) {
-    cssStyles = [...cssStyles, newStyles]
+    cssStyles = [...cssStyles, newStyles];
   }
-  const newIcon = mdDirectory.getFile(config.icon)
+  const newIcon = mdDirectory.getFile(config.icon);
   if (newIcon) {
-    icon = newIcon.name
+    icon = newIcon.name;
   }
 
   // Copy files to new directory
   mdDirectory.files.forEach((file) => {
-    if (file.name.endsWith('.html')) return
+    if (file.name.endsWith('.html')) return;
     // Markdown files are converted to html
     if (file.name.endsWith('.md')) {
-      const data = file.data.replaceAll('%20', '__SPACE__')
-      let htmlRender = md.render(data)
-          .replaceAll('.md', '.html')
-          .replaceAll('__SPACE__', ' ')
-          .replaceAll('&quot;', '"')
+      const data = file.data.replaceAll('%20', '__SPACE__');
+      let htmlRender = md
+        .render(data)
+        .replaceAll('.md', '.html')
+        .replaceAll('__SPACE__', ' ')
+        .replaceAll('&quot;', '"');
       // TODO: Add all tags
       for (let tag of ['div', 'p', 'a', 'span', 'iframe']) {
         htmlRender = htmlRender
-            .replaceAll(`&gt;\n&lt;/${tag}&gt;`, `>\n</${tag}>`)
-            .replaceAll(`&gt;&lt;/${tag}&gt;`, `></${tag}>`)
-            .replaceAll(`&lt;${tag}&gt;`, `<${tag}>`)
-            .replaceAll(`&lt;/${tag}&gt;`, `</${tag}>`)
-            .replaceAll(`&lt;${tag}`, `<${tag}`)
-            // TODO: Catch more cases
+          .replaceAll(`&gt;\n&lt;/${tag}&gt;`, `>\n</${tag}>`)
+          .replaceAll(`&gt;&lt;/${tag}&gt;`, `></${tag}>`)
+          .replaceAll(`&lt;${tag}&gt;`, `<${tag}>`)
+          .replaceAll(`&lt;/${tag}&gt;`, `</${tag}>`)
+          .replaceAll(`&lt;${tag}`, `<${tag}`);
+        // TODO: Catch more cases
       }
-      let htmlStyles = ''
+      let htmlStyles = '';
       cssStyles.forEach((style) => {
-        htmlStyles += `<link rel="stylesheet" href="${style}">\n  `
-      })
+        htmlStyles += `<link rel="stylesheet" href="${style}">\n  `;
+      });
       // Inserts
       const htmlData = htmlLayout
-          .replaceAll(config.insertMarkdown, htmlRender)
-          .replaceAll(config.insertStyles, htmlStyles)
-          .replaceAll(config.insertTitle, 
-              file.metadata.title ? file.metadata.title : file.name)
-          .replaceAll(config.insertIcon, icon)
-      const htmlFile = new File(file.name.replaceAll('.md', '.html'), htmlData)
-      htmlFile.metadata = file.metadata
-      htmlDirectory.files.push(htmlFile)
+        .replaceAll(config.insertMarkdown, htmlRender)
+        .replaceAll(config.insertStyles, htmlStyles)
+        .replaceAll(
+          config.insertTitle,
+          file.metadata.title ? file.metadata.title : file.name
+        )
+        .replaceAll(config.insertIcon, icon);
+      const htmlFile = new File(file.name.replaceAll('.md', '.html'), htmlData);
+      htmlFile.metadata = file.metadata;
+      htmlDirectory.files.push(htmlFile);
     } else {
-      htmlDirectory.files.push(new File(file.name, file.data))
+      htmlDirectory.files.push(new File(file.name, file.data));
     }
-  })
+  });
 
   // Recursively create html subdirectories
-  const subDirectoryStyles = []
+  const subDirectoryStyles = [];
   cssStyles.forEach((style) => {
-    subDirectoryStyles.push('../' + style)
-  })
-  icon = '../' + icon
+    subDirectoryStyles.push('../' + style);
+  });
+  icon = '../' + icon;
   mdDirectory.directories.forEach((mdSubDir) => {
     htmlDirectory.directories.push(
-        buildHtml(mdSubDir, htmlLayout, subDirectoryStyles, icon))
-  })
+      buildHtml(mdSubDir, htmlLayout, subDirectoryStyles, icon)
+    );
+  });
 
-  return htmlDirectory
+  return htmlDirectory;
 }
 
-
 /**
- * 
- * @param {Directory} directory 
- * @param {Directory} parentDirectory 
+ *
+ * @param {Directory} directory
+ * @param {Directory} parentDirectory
  */
-function linkMarkdown(directory, parentDirectory=null) {
+function linkMarkdown(directory, parentDirectory = null) {
   // Create link to directory index.md
   directory.files.forEach((file) => {
-    if (file.name === 'index.md' || !file.name.endsWith('.md')) return
-    file.data = `↩️ [${directory.name}](./index.md)\n` + file.data
-  })
+    if (file.name === 'index.md' || !file.name.endsWith('.md')) return;
+    file.data = `↩️ [${directory.name}](./index.md)\n` + file.data;
+  });
 
   // Get existing index.md
-  let indexFile = directory.getFile(/^index.md$/)
-  const indexData = indexFile ? indexFile.data : ''
-  directory.removeFile(indexFile)
-  let indexMd = ''
+  let indexFile = directory.getFile(/^index.md$/);
+  const indexData = indexFile ? indexFile.data : '';
+  directory.removeFile(indexFile);
+  let indexMd = '';
 
   // Create link to parent directory index.md
   if (parentDirectory) {
-    indexMd += `↩️ [${parentDirectory.name}](../index.md)\n`
+    indexMd += `↩️ [${parentDirectory.name}](../index.md)\n`;
   }
-  
+
   // Create header
-  indexMd += `\n# ${directory.name}\n\n`
+  indexMd += `\n# ${directory.name}\n\n`;
 
   // Create links to subdirectories
   if (directory.directories.length) {
-    indexMd += `### Directories\n`
+    indexMd += `### Directories\n`;
     directory.directories.forEach((directory) => {
-      indexMd += `- [${directory.name}](./${directory.name}/index.md)\n`
-    })
+      indexMd += `- [${directory.name}](./${directory.name}/index.md)\n`;
+    });
   }
 
   // Create links to files
   if (directory.files.length) {
-    let count = 0
+    let count = 0;
     directory.files.forEach((file) => {
-      if (file.name == config.htmlLayout
-          || file.name == config.cssStyles) return
+      if (file.name == config.htmlLayout || file.name == config.cssStyles)
+        return;
       if (!count) {
-        indexMd += `### Files\n`
-        count++
+        indexMd += `### Files\n`;
+        count++;
       }
-      indexMd += `- [${file.name}](${file.name.replaceAll(' ', '%20')})\n`
-    })
+      indexMd += `- [${file.name}](${file.name.replaceAll(' ', '%20')})\n`;
+    });
   }
 
   // Add index.md (back) to directory
   if (indexFile) {
-    indexFile.data = indexMd + indexData
+    indexFile.data = indexMd + indexData;
   } else {
-    indexFile = new File('index.md', indexMd + indexData)
-    indexFile.metadata = {title: directory.name}
+    indexFile = new File('index.md', indexMd + indexData);
+    indexFile.metadata = { title: directory.name };
   }
-  directory.files.push(indexFile)
-  
+  directory.files.push(indexFile);
+
   // Recursively link subdirectories
   directory.directories.forEach((subDirectory) => {
-    linkMarkdown(subDirectory, directory)
-  })
+    linkMarkdown(subDirectory, directory);
+  });
 }
 
 function readMetadata(directory) {
   // Read metadata
   directory.files.forEach((file) => {
     if (file.name.endsWith('.md')) {
-      const result = metadataParser(file.data)
-      file.data = result.content
-      file.metadata = result.metadata
+      const result = metadataParser(file.data);
+      file.data = result.content;
+      file.metadata = result.metadata;
     }
-  })
-  
+  });
+
   // Recursively read metadata in subdirectories
   directory.directories.forEach((subDirectory) => {
-    readMetadata(subDirectory)
-  })
+    readMetadata(subDirectory);
+  });
 }
